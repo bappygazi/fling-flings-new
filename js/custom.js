4968354a19c26498c467e5d63316197c4e83387b
sakaethossain <sakaethossain122@gmail.com>
2022-02-16 07:32:22 +0600

    hgkj

--- js/custom.js
+++ js/custom.js
@@ -3,21 +3,21 @@ $(function () {
     scrollingSpeed: 1500,
   });
 
-  $('.down-icon a[href^="#"]').on("click", function (e) {
-    e.preventDefault();
-    var target = this.hash;
-    $target = $(target);
-    $("html, body")
-      .stop()
-      .animate(
-        {
-          scrollTop: $target.offset().top - 0, //no need of parseInt here
-        },
-        1200,
-        "swing",
-        function () {
-          window.location.hash = target;
-        }
-      );
-  });
+  // $('.down-icon a[href^="#"]').on("click", function (e) {
+  //   e.preventDefault();
+  //   var target = this.hash;
+  //   $target = $(target);
+  //   $("html, body")
+  //     .stop()
+  //     .animate(
+  //       {
+  //         scrollTop: $target.offset().top - 0, //no need of parseInt here
+  //       },
+  //       1200,
+  //       "swing",
+  //       function () {
+  //         window.location.hash = target;
+  //       }
+  //     );
+  // });
 });
