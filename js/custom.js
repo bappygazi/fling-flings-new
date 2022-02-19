05f623b184bb3f71bda5acf9bb8351caf5b62fdc
sakaethossain <sakaethossain122@gmail.com>
2022-02-20 02:18:59 +0600

    adfsaas

--- js/custom.js
+++ js/custom.js
@@ -2,22 +2,4 @@ $(function () {
   new fullpage("#fullpage", {
     scrollingSpeed: 1500,
   });
-
-  // $('.down-icon a[href^="#"]').on("click", function (e) {
-  //   e.preventDefault();
-  //   var target = this.hash;
-  //   $target = $(target);
-  //   $("html, body")
-  //     .stop()
-  //     .animate(
-  //       {
-  //         scrollTop: $target.offset().top - 0, //no need of parseInt here
-  //       },
-  //       1200,
-  //       "swing",
-  //       function () {
-  //         window.location.hash = target;
-  //       }
-  //     );
-  // });
 });
